402f72f0e87b99485b45b7d0a533e9d3090a5601
david-etag <107001240+david-etag@users.noreply.github.com>
2022-06-09 17:03:26 -0300

    hasura

--- functions/products.js
+++ functions/products.js
@@ -1,6 +1,19 @@
-const products = require("../data/products.json")
+const { query } = require('./util/hasura');
 
 exports.handler = async () => {
+    const { products } = await query({
+        query: `
+            query AllProducts {
+                products {
+                    id
+                    name
+                    img
+                    price
+                    description
+                }
+            }
+        `
+    })
     return {
         statusCode: 200,
         body: JSON.stringify(products)
